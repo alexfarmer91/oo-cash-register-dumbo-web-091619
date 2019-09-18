111ce78cdb45cc38f140e1917ccd172f56ec3381
Alex Farmer <farmale10@gmail.com>
2019-09-18 21:06:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,12 +9,11 @@ class CashRegister
     @items = []
   end
 
-   transaction_prices = []
-   
+
   def add_item(title,price,quantity = 1)
     if quantity>1
       i=0
-      while i<quantity
+      while i < quantity
         @items.push(title)
         i+=1
       end
