18a80f7daf9c399906f0329665e0395628c7b864
Alex Farmer <farmale10@gmail.com>
2019-09-18 20:48:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
   attr_reader
   attr_accessor :total, :employee_discount
 
-  def initialize(total=0, employee_discount)
+  def initialize(total=0, employee_discount = nil)
     @total = total
     
   end 
